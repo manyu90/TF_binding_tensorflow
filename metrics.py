38abe1aca78ccc6e7d9e655d6e9b0e11de257e65
manyu90 <manyu@stanford.edu>
2018-02-11 18:20:23 -0800

    removed incorrect loss metric and fixed __str__

--- metrics.py
+++ metrics.py
@@ -4,8 +4,7 @@ from collections import OrderedDict
 from sklearn.metrics import auc, log_loss, precision_recall_curve, roc_auc_score
 
 
-def loss(labels, predictions):
-    return log_loss(labels, predictions)
+
 
 
 def positive_accuracy(labels, predictions, threshold=0.5):
@@ -38,36 +37,27 @@ def recall_at_precision_threshold(labels, predictions, precision_threshold):
 class ClassificationResult(object):
 
     def __init__(self, labels, predictions, task_names=None):
-        assert labels.dtype == bool
-        self.results = [OrderedDict((
-            ('Loss', loss(task_labels, task_predictions)),
+        
+        self.results = OrderedDict((
+            
             ('Balanced accuracy', balanced_accuracy(
-                task_labels, task_predictions)),
-            ('auROC', auROC(task_labels, task_predictions)),
-            ('auPRC', auPRC(task_labels, task_predictions)),
+                labels,predictions)),
+            ('auROC', auROC(labels,predictions)),
+            ('auPRC', auPRC(labels,predictions)),
             ('Recall at 5% FDR', recall_at_precision_threshold(
-                task_labels, task_predictions, 0.95)),
+                labels,predictions, 0.95)),
             ('Recall at 10% FDR', recall_at_precision_threshold(
-                task_labels, task_predictions, 0.9)),
+                labels,predictions, 0.9)),
             ('Recall at 20% FDR', recall_at_precision_threshold(
-                task_labels, task_predictions, 0.8)),
-            ('Num Positives', task_labels.sum()),
-            ('Num Negatives', (1 - task_labels).sum())
-        )) for task_labels, task_predictions in zip(labels.T, predictions.T)]
-        self.task_names = task_names
-        self.multitask = labels.shape[1] > 1
-
+                labels,predictions, 0.8)),
+            ('Num Positives', labels.sum()),
+            ('Num Negatives', (1 - labels).sum())))
+        
     def __str__(self):
-        return '\n'.join(
-            '{}Loss: {:.4f}\tBalanced Accuracy: {:.2f}%\t '
+        return ('Balanced Accuracy: {:.2f}%\t '
             'auROC: {:.3f}\t auPRC: {:.3f}\n\t'
             'Recall at 5%|10%|20% FDR: {:.1f}%|{:.1f}%|{:.1f}%\t '
-            'Num Positives: {}\t Num Negatives: {}'.format(
-                '{}: '.format('Task {}'.format(
-                    self.task_names[task_index]
-                    if self.task_names is not None else task_index))
-                if self.multitask else '', *results.values())
-            for task_index, results in enumerate(self.results))
+            'Num Positives: {}\t Num Negatives: {}'.format(*self.results.values()))
+            
 
-    def __getitem__(self, item):
-        return np.array([task_results[item] for task_results in self.results])
+    
